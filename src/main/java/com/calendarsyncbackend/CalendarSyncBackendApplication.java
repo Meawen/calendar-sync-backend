0d5379b94f6924635d8c39261fc6448d12ff78bd
lmiholic <lukas.miholiclol@gmail.com>
2024-10-07 23:35:41 +0200

    spring boot init

---
+++ src/main/java/com/calendarsyncbackend/CalendarSyncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.calendarsyncbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarSyncBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarSyncBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/calendarsyncbackend/CalendarSyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calendarsyncbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarSyncBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
