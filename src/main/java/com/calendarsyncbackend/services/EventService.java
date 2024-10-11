5cef03f0933f78fa9c980e1c25ef287178eede21
lmiholic <lukas.miholiclol@gmail.com>
2024-10-11 12:22:38 +0200

    EventService ++

---
+++ src/main/java/com/calendarsyncbackend/services/EventService.java
@@ -0,0 +1,50 @@
+package com.calendarsyncbackend.services;
+
+import com.calendarsyncbackend.models.Event;
+import com.calendarsyncbackend.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EventService {
+
+    private final EventRepository eventRepository;
+
+    @Autowired
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
+    public Optional<Event> getEventById(int id) {
+        return eventRepository.findById(id);
+    }
+    public Page<Event> getEventsPagable(Pageable pageable) {
+        return eventRepository.findAll(pageable);
+    }
+    public Event createEvent(Event event) {
+        return eventRepository.save(event);
+    }
+    public Event updateEvent(int id, Event event) {
+        Optional<Event> eventOptional = eventRepository.findById(id);
+        if (eventOptional.isPresent()) {
+            eventOptional.get().setName(event.getName());
+            eventOptional.get().setDescription(event.getDescription());
+            eventOptional.get().setDatetimeFrom(event.getDatetimeFrom());
+            eventOptional.get().setDatetimeTo(event.getDatetimeTo());
+            return eventRepository.save(eventOptional.get());
+        }
+        System.out.println("Something went wrong, id not found");
+        return null;
+    }
+    public void deleteEvent(int id) {
+        eventRepository.deleteById(id);
+    }
+}
