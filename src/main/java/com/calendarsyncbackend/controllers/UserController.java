aaaf76d89057ddaa22600d825cbd3b1da8fb2f68
lmiholic <lukas.miholiclol@gmail.com>
2024-10-11 08:51:43 +0200

    constructor injection change

--- src/main/java/com/calendarsyncbackend/controllers/UserController.java
+++ src/main/java/com/calendarsyncbackend/controllers/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,8 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+    private final UserService userService;
+
+    // Constructor-based dependency injection
     @Autowired
-    private UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
     public Page<User> getUserPage(@RequestParam(defaultValue = "0") int page,
@@ -35,3 +39,4 @@ public class UserController {
 
 
 }
+
