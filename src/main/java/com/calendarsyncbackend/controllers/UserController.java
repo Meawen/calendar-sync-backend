70a4a285ad83b3b73de04cd2b8f572013ed52918
lmiholic <lukas.miholiclol@gmail.com>
2024-10-12 16:42:30 +0200

    UserController finished(?)

--- src/main/java/com/calendarsyncbackend/controllers/UserController.java
+++ src/main/java/com/calendarsyncbackend/controllers/UserController.java
@@ -1,16 +1,19 @@
 package com.calendarsyncbackend.controllers;
 
 import com.calendarsyncbackend.models.User;
+import com.calendarsyncbackend.repositories.UserRepository;
 import com.calendarsyncbackend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
@@ -22,6 +25,43 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.findAllUsers();
+        if (users.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable int id) {
+        return userService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable int id, @RequestBody User user) {
+        User userUpdated = userService.updateUser(id, user);
+        return ResponseEntity.ok(userUpdated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable int id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build(); //returns 204 if it works
+    }
+
 
     @GetMapping
     public Page<User> getUserPage(@RequestParam(defaultValue = "0") int page,
@@ -36,6 +76,8 @@ public class UserController {
         return userService.findAllUsersPageable(pageable);
 
         }
+
+
 
 
 }
