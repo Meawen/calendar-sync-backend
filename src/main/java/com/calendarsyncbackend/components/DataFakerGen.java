0935d67334e46201852c5bed8e90981aa22d00ec
lmiholic <lukas.miholiclol@gmail.com>
2024-10-28 17:35:17 +0100

    Data generated kinda(?) -> only user and event

---
+++ src/main/java/com/calendarsyncbackend/components/DataFakerGen.java
@@ -0,0 +1,54 @@
+package com.calendarsyncbackend.components;
+
+import com.calendarsyncbackend.models.Event;
+import com.calendarsyncbackend.models.User;
+import com.calendarsyncbackend.repositories.EventRepository;
+import com.calendarsyncbackend.repositories.UserRepository;
+import net.datafaker.Faker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import java.time.Instant;
+import java.util.stream.IntStream;
+
+@Component
+public class DataFakerGen implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final EventRepository eventRepository;
+
+    private final Faker faker = new Faker();
+
+    @Autowired
+    public DataFakerGen(UserRepository userRepository, EventRepository eventRepository) {
+        this.userRepository = userRepository;
+        this.eventRepository = eventRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        IntStream.range(1, 20).forEach(i -> {
+            User user = new User();
+            user.setUsername(faker.internet().domainWord());
+            user.setName(faker.name().fullName());
+            user.setEmail(faker.internet().emailAddress());
+            user.setPassword(faker.internet().password());
+            user.setRole(faker.number().numberBetween(1, 3));
+            userRepository.save(user);
+            System.out.println("Saved User: " + user.getUsername() + " with email: " + user.getEmail());
+        });
+
+        IntStream.range(1, 10).forEach(i -> {
+            Event event = new Event();
+            event.setName(faker.esports().event());
+            event.setDescription(faker.lorem().sentence());
+            event.setDatetimeFrom(Instant.now().minusSeconds(faker.number().numberBetween(3600, 2592000)));
+            event.setDatetimeTo(event.getDatetimeFrom().plusSeconds(faker.number().numberBetween(3600, 86400)));
+            eventRepository.save(event);
+            System.out.println("Saved Event: " + event.getName() + " from: " + event.getDatetimeFrom() + " to: " + event.getDatetimeTo());
+        });
+
+
+
+    }
+}
